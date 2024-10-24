e4f68ffdcce5762f34f9ca9276c0a691d7555256
Zacarihaz <jsarasquetacastinei@itba.edu.ar>
2024-10-24 19:07:56 -0300

    avance

--- Modulos/Apps.py
+++ Modulos/Apps.py
@@ -1,51 +1,55 @@
 from Telefono import *
-from prueba3 import nombre
+from datetime import datetime
+from collections import deque
+
 
 
 class App:
+    apps_existentes = []
     def __init__(self, id_app, espacio, nombre_app):
         self.id_app = id_app
         self.espacio = espacio
         self.nombre = nombre_app
 
+        App.apps_existentes.append(self.id_app)
+
 #Descargar una nueva app desde la tienda de aplicaciones.
 class AppStore(App):
     apps_instaladas = {}    #aca poner todas las apps que vienen por default
-    contador  = 0
+    espacio_libre  = 0
 
-    @staticmethod
-    def espacio_actual():
-
-
-    def __init__(self, id_app, espacio, id_user: Telefono, nombre_app): #verificar que hay espacio en el celular
+    def __init__(self, id_app, espacio, id_user,nombre_app): #verificar que hay espacio en el celular
         super().__innit__(espacio, id_app, nombre_app)
+        if  AppStore.espacio_libre < espacio :
+            raise ValueError("No hay espacio suficiente")
+        if id_app not in App.apps_existentes:
+            raise ValueError("Esta app no existe")
         self.id_user = id_user
         self.id_app = id_app
         self.nombre_app = nombre_app
+        self.espacio = espacio
 
         AppStore.apps_instaladas[self.id_app] = self
-        AppStore.contador +=1
+        AppStore.espacio_libre -= self.espacio
 
     def borrar_app(self, id_app):                                       #poner condicion que las apps default no se borren
         app_borrada = AppStore.apps_instaladas.pop(self.id_app)
+        AppStore.espacio_libre += self.espacio
         print(f'{app_borrada} fue eliminada')
+
+    @classmethod
+    def configurar_espacio_libre(cls, telefono: Telefono):
+        cls.espacio_libre = telefono.almacenamiento  # Accede al atributo de instancia
 
 
 class Contactos():
-    contactos_guardados ={}
-    def __init__(self, nombre, numero, correo ):
-        if numero not in Telefono.numeros_registrados:
-            raise ValueError("El telefono guardado no existe")
-        super().__init__()
-        self.nombre = nombre
-        self.numero= numero
-        self.correo = correo
-        Contactos.contactos_guardados[self.nombre] = self
+    def __init__(self):
+        self.contactos_guardados= {}
+    def agregar_contacto(self, nombre, numero, correo ):
+        if numero not in self.contactos_guardados.keys():
+            self.contactos_guardados[numero] = Contacto(nombre, numero, correo)
 
-    def actualizar_contacto(self, nuevo_nombre, nuevo_numero, nuevo_correo):
-        self.nombre = nuevo_nombre
-        self.numero = nuevo_numero
-        self.correo = nuevo_correo
+
 
 class TelefonoApp():
     def __init__(self, ):
@@ -70,22 +74,53 @@ class TelefonoApp():
         print(f'Cortaste la llamada del {numero}')
 
 
-class MensajesApp():
-    def __init__(self):
+#class MensajesApp():
+ #   def __init__(self):
 
-    def enviar_mensaje(self, mensaje, numero):
-        print(f'Enviando mensaje: {mensaje} a numero: {numero}')#e. Enviar y recibir mensajes de texto (SMS) a un número de destino.'
-
-
-    def recibir_mensaje(self, mensaje, numero):
-        print(f'Recibiendo mensaje: {mensaje} de numero: {numero}')#f. Ver bandeja de entrada de SMS e historial de llamadas.'
+  #  def enviar_mensaje(self, mensaje, numero):
+   #     print(f'Enviando mensaje: {mensaje} a numero: {numero}')#e. Enviar y recibir mensajes de texto (SMS) a un número de destino.'
 
 
-    def eliminar_mensaje(self, mensaje):
-        print(f'Eliminando mensaje: {mensaje}')#g. Eliminar mensajes (SMS)'
+    #def recibir_mensaje(self, mensaje, numero):
+     #   print(f'Recibiendo mensaje: {mensaje} de numero: {numero}')#f. Ver bandeja de entrada de SMS e historial de llamadas.'
 
 
-try:
-    if __name__ == '__main__':
-        mi_telefono = Telefono(2, "jose", "nokia", "ios", 12, 8, 87654321)
-        JoseFranciscoITBA = Contactos("Jose Francisco ITBA",  87654321, "jsarasqueta@hotmail.com")
+    #def eliminar_mensaje(self, mensaje):
+     #   print(f'Eliminando mensaje: {mensaje}')#g. Eliminar mensajes (SMS)'
+class mailApp(App):
+   def __init__(self, espacio, id_app, nombre_app):
+       super().__innit__(espacio, id_app, nombre_app)
+       self.recibidos = []
+       self.enviados = []
+
+   def pantalla_de_inicio(self):
+       for i in range(len(self.recibidos),-1):
+           print(self.recibidos[i])
+
+
+class Email():
+    def __init__(self, mail_id, remitente, destinatario, asunto, contenido):
+        self.mail.id = mail_id
+        self.remitente = remitente
+        self.destinatario = destinatario
+        self.asunto = asunto
+        self.contenido = contenido
+        self.fecha = datetime.now()
+        self.leido = False
+
+    def marcar_como_leido(self):
+        self.leido = True
+
+    def obtener_info(self):
+        return f"Asunto: {self.asunto}, De: {self.remitente}, Para: {self.destinatario}, Fecha: {self.fecha}, Leído: {self.leido}"
+
+
+#try:
+ #   if __name__ == '__main__':
+  #      mi_telefono = Telefono(2, "jose", "nokia", "ios", 12, 8, 87654321)
+   #     JoseFranciscoITBA = Contactos("Jose Francisco ITBA",  87654321, "jsarasqueta@hotmail.com")
+
+#except ValueError as e:
+ #   print(e)
+
+
--- Modulos/Telefono.py
+++ Modulos/Telefono.py
@@ -1,12 +1,21 @@
+#from TP.EDP_TP2.Modulos.Apps import AppStore
+from EDP_TP2.Modulos.Apps import *
 
+
+
+#Un teléfono celular tiene al menos los siguientes atributos:
+#ID (único), Nombre, Modelo, Sistema Operativo y versión, capacidad de memoria RAM, capacidad de
+#almacenamiento y número telefónico.
 class Telefono:
 
     numeros_registrados = []
     numeros_conectados = []
 
-    def __init__(self, id_telefono, nombre, modelo, os, version_os, ram, almacenamiento, numero, estado = 0, estado_pantalla = 0, estado_red=0):
+    def __init__(self,  id_telefono, nombre, modelo, os, version_os, ram, almacenamiento, numero, espacio_libre, estado = 0, estado_pantalla = 0, estado_red=0): #PONER CONFIGURACION, mensajes_app
         if len(str(numero)) != 8:
             raise ValueError("El numero ingresado es inválido") #a nacho no le funciono pero ahora si
+        if espacio_libre > almacenamiento:
+            raise ValueError("Espacio libre debe ser menor al almacenamiento")
         self.id_telefono = id_telefono
         self.nombre = nombre
         self.modelo = modelo
@@ -14,11 +23,18 @@ class Telefono:
         self.version_os = version_os #sistema operativo
         self.ram = ram
         self.almacenamiento = almacenamiento
+        self.espacio_libre = espacio_libre
         self.numero = numero
         self.estado = estado
         self.estado_pantalla = estado_pantalla
         self.estado_red = estado_red
+        self.contactos = Contactos()
+        self.telefono_app = Telefono()
+        self.appstore = AppStore()
+
+
         Telefono.numeros_registrados.append(self.numero)
+
 
     def __str__(self):
         return f'(nombre: {self.nombre}, modelo: {self.modelo} , numero: {self.numero})'
@@ -56,11 +72,20 @@ class Telefono:
         else: #si el celular está apagado
             print(f" {self.numero}: Para conectar a la red debe encenderse el telefono ")
 
+class Contacto():
+    def __init__(self, nombre, numero, correo ):
+        self.nombre = nombre
+        self.numero= numero
+        self.correo = correo
 
+    def actualizar_contacto(self, nuevo_nombre, nuevo_numero, nuevo_correo):
+        self.nombre = nuevo_nombre
+        self.numero = nuevo_numero
+        self.correo = nuevo_correo
 try:
     if __name__=='__main__':
-        telefono_nacho = Telefono("Nacho", "nokia", "ios", 12, 8, 500, 12345678)
-        mi_telefono = Telefono(2, "jose", "nokia", "ios", 12, 8, 87654321)
+        telefono_nacho = Telefono(12, "Nacho", "Iphone", "X", "IOS", 20, 500, 12345678, 400 )
+        mi_telefono = Telefono(2, "Jose", "Iphone", "X", "IOS", 20, 600, 87654321, 400)
         print(telefono_nacho)
         print(Telefono.numeros_registrados)
         mi_telefono.on_off()
